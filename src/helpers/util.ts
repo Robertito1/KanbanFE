4d140884472e9bb39930fb231f8cee4e52955a3e
Robert <orazurobert@yahoo.com>
2021-08-25 11:13:51 +0100

    create-react-app

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,1 +1,1 @@
-export const baseUrl = 'https://shielded-ravine-63689.herokuapp.com//https://intense-retreat-24063.herokuapp.com';
+export const baseUrl = 'https://shielded-ravine-63689.herokuapp.com/https://intense-retreat-24063.herokuapp.com';
